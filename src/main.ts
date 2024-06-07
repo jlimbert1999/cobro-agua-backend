63fbfcdd3bfe8b792360b0e4b190f63d3cb42bca
josel <josel@gmail.com>
2024-06-07 14:32:37 -0400

    despliegue del front

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
